26f35b5f85e1f5c1bc1f78208409610c39852600
F9UF <atoseka@gmail.com>
2020-02-04 14:06:32 +0700

    :pencil:  update comment in problem2

--- [2] floating-prime/main.go
+++ [2] floating-prime/main.go
@@ -11,13 +11,18 @@ import (
 
 func main() {
 	fmt.Printf("Enter Floating Prime: ")
+	// read line until user type "0.0"
 	for x, _ := bufio.NewReader(os.Stdin).ReadString('\n'); strings.Trim(x, "\r\n") != "0.0"; x, _ = bufio.NewReader(os.Stdin).ReadString('\n') {
+
+		// trim string and remove "." out
 		number := strings.Trim(x, "\r\n")
 		number = strings.Join(strings.Split(number, "."), "")
 
 		var result string
 		result = "FALSE"
 
+		// loop check prime of number until found the prime number
+		// Loop sinc one floating point to 3 floating point
 		for i := 2; i <= 4; i++ {
 			substr := number[0:i]
 			if x, err := strconv.Atoi(substr); err == nil {
@@ -34,6 +39,9 @@ func main() {
 	}
 }
 
+/*
+	isPrime is a function to check prime of input number
+*/
 func isPrime(number int) bool {
 	for i := 2; i <= int(math.Floor(float64(number)/2)); i++ {
 		if number%i == 0 {
